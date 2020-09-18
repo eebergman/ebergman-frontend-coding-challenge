f836084c393688c776471b797e494a8190b35385
Erin <erin@Erins-MacBook-Pro.local>
2020-09-18 14:16:10 -0400

    Set up enrollee and test enrollee

---
+++ src/app/shared/models/enrollee.ts
@@ -0,0 +1,6 @@
+export class Enrollee {
+  active: boolean;
+  dateOfBirth: string;
+  id: string;
+  name: string;
+}
---
+++ src/test/test-enrollee.ts
@@ -0,0 +1,8 @@
+import { Enrollee } from 'src/app/shared/models/enrollee';
+
+export const TEST_GOOD_ENROLLEE: Enrollee = {
+  active: true,
+  dateOfBirth: '1962-11-3',
+  id: '36653835-fbe0-4c42-a93c-3e561823934f',
+  name: 'Kate Newell',
+};
