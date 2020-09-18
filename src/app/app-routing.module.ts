f65ef3c0fdec2991acfb853ee321ae60051e8f31
Erin <erin@Erins-MacBook-Pro.local>
2020-09-18 16:42:58 -0400

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EnrolleeComponent } from './shared/components/enrollee/enrollee.component';
+import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
+import { LayoutComponent } from './shared/components/layout/layout/layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: EnrolleeComponent,
+      },
+    ],
+  },
+  {
+    path: '404',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-
+import { EnrolleeComponent } from './components/enrollee/enrollee.component';
+import { NotFoundComponent } from './components/errors/not-found/not-found.component';
+import { LayoutComponent } from './components/layout/layout/layout.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NotFoundComponent, EnrolleeComponent, LayoutComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [LayoutComponent, EnrolleeComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class SharedModule { }
+export class SharedModule {}
