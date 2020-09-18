d8ad07977b0940090a58479b174e6f4e8111cd4f
Erin <erin@Erins-MacBook-Pro.local>
2020-09-18 14:25:02 -0400

    Add httpClientModel and service to appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EnrolleeService } from './core/services/enrollee.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [EnrolleeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
