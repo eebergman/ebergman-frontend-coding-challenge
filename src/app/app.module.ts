5505c3985f4ddd438fbfced28b776c489e722da3
Erin <erin@Erins-MacBook-Pro.local>
2020-09-18 15:42:01 -0400

    Correct pathing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 import { EnrolleeService } from './core/services/enrollee.service';
 import { MaterialModule } from './modules/material/material.module';
-import { CoreModule } from './core/core/core.module';
-import { SharedModule } from './shared/shared/shared.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
