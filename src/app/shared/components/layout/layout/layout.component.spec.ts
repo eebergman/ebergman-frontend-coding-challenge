019afc1f3aeb77dcb0a5e2ac02ee31404265bf30
Erin <erin@Erins-MacBook-Pro.local>
2020-09-20 13:02:04 -0400

    Testing aids

--- src/app/shared/components/layout/layout/layout.component.spec.ts
+++ src/app/shared/components/layout/layout/layout.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LayoutComponent } from './layout.component';
@@ -8,9 +9,9 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
-    })
-    .compileComponents();
+      declarations: [LayoutComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
---
+++ src/test/mocks/mockEnrolleeService.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { EnrolleeService } from 'src/app/core/services/enrollee.service';
+import { Enrollee } from 'src/app/shared/models/enrollee';
+import { TEST_GOOD_ENROLLEE_ACTIVE } from '../test-enrollee';
+
+@Injectable()
+export class MockEnrolleeService extends EnrolleeService {
+  callEnrollees(): Observable<Enrollee> {
+    return of(TEST_GOOD_ENROLLEE_ACTIVE);
+  }
+}
