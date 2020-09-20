e14e325e956ca6080774cacdd66500edc56c79b7
Erin <erin@Erins-MacBook-Pro.local>
2020-09-20 16:26:00 -0400

    Refactor EnrolleeService

--- src/test/mocks/mockEnrolleeService.ts
+++ src/test/mocks/mockEnrolleeService.ts
@@ -3,11 +3,11 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { EnrolleeService } from 'src/app/core/services/enrollee.service';
 import { Enrollee } from 'src/app/shared/models/enrollee';
-import { TEST_GOOD_ENROLLEE_ACTIVE } from '../test-enrollee';
+import { TEST_GOOD_ENROLLEE_ACTIVE } from '../models/test-enrollee';
 
 @Injectable()
 export class MockEnrolleeService extends EnrolleeService {
-  fetchEnrollees(): Observable<Enrollee> {
-    return of(TEST_GOOD_ENROLLEE_ACTIVE);
+  fetchEnrollees(): Observable<[Enrollee]> {
+    return of([TEST_GOOD_ENROLLEE_ACTIVE]);
   }
 }
