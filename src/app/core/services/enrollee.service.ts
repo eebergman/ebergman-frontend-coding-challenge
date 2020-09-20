8599b2319f0c70f7adea8d938fc013cdc27c0aef
Erin <erin@Erins-MacBook-Pro.local>
2020-09-20 12:19:34 -0400

    Add more typing to enrollee service

--- src/app/core/services/enrollee.service.ts
+++ src/app/core/services/enrollee.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs';
+import { Enrollee } from 'src/app/shared/models/enrollee';
 import { ApiBaseService } from './api-base.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EnrolleeService extends ApiBaseService {
-  public callEnrollees(): Observable<any> {
+  public callEnrollees(): Observable<Enrollee> {
     return this.callGet('enrollees');
   }
 }
