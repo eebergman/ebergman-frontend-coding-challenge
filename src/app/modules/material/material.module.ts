fa75fd32e9b6e2730a25f2900db704e65873d08d
Erin <erin@Erins-MacBook-Pro.local>
2020-09-20 13:12:52 -0400

    Add Header

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const components = [
   MatTableModule,
@@ -12,6 +13,7 @@ const components = [
   MatSortModule,
   MatFormFieldModule,
   MatInputModule,
+  MatToolbarModule,
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,16 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../modules/material/material.module';
 import { EnrolleeComponent } from './components/enrollee/enrollee.component';
 import { NotFoundComponent } from './components/errors/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './components/layout/layout/layout.component';
 
 @NgModule({
-  declarations: [NotFoundComponent, EnrolleeComponent, LayoutComponent],
+  declarations: [
+    NotFoundComponent,
+    EnrolleeComponent,
+    LayoutComponent,
+    HeaderComponent,
+  ],
   imports: [CommonModule, RouterModule, MaterialModule],
   exports: [LayoutComponent, EnrolleeComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
