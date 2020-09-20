448fd7a602c728e546654bdc25713ee721149de3
Erin <erin@Erins-MacBook-Pro.local>
2020-09-20 13:01:34 -0400

    Update Mat Mod

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,10 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
-const components = [MatTableModule, MatPaginatorModule];
+const components = [
+  MatTableModule,
+  MatCardModule,
+  MatSortModule,
+  MatFormFieldModule,
+  MatInputModule,
+];
 
 @NgModule({
   declarations: [],
