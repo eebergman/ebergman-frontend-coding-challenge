e8655725a7df3b216887117c64f2363bc95b1a7e
Erin <erin@Erins-MacBook-Pro.local>
2020-09-21 09:41:02 -0400

    Add the backend service

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 21 September 2020
 Weekend Code Challenge completed over the better part of two days.
+
+Please find the backend service in the Frontend coding challenge directory
--- src/app/shared/components/edit-dialog/edit-dialog.component.spec.ts
+++ src/app/shared/components/edit-dialog/edit-dialog.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from 'src/app/modules/material/material.module';
@@ -10,8 +10,12 @@ import { EditDialogComponent } from './edit-dialog.component';
 describe('EditDialogComponent', () => {
   let component: EditDialogComponent;
   let fixture: ComponentFixture<EditDialogComponent>;
+  let mockDialogRef;
 
   beforeEach(async () => {
+    mockDialogRef = {
+      close: jasmine.createSpy('close'),
+    };
     await TestBed.configureTestingModule({
       imports: [NoopAnimationsModule, ReactiveFormsModule, MaterialModule],
       declarations: [EditDialogComponent],
@@ -20,6 +24,7 @@ describe('EditDialogComponent', () => {
           provide: MAT_DIALOG_DATA,
           useValue: { enrollee: TEST_GOOD_ENROLLEE_ACTIVE },
         },
+        { provide: MatDialogRef, useValue: mockDialogRef },
       ],
     }).compileComponents();
   });
